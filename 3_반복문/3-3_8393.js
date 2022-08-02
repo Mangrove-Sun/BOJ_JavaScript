64bb28be68c5304fe95f87cca6b11968987452a8
Mangrove-Sun <tjsghd0317@gmail.com>
2022-08-02 21:53:07 +0900

    No.8393 읽어올 파일 경로 수정

--- 3_반복문/3-3_8393.js
+++ 3_반복문/3-3_8393.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const input = fs.readFileSync('input.txt', 'utf8').toString();
+const input = fs.readFileSync('/dev/stdin', 'utf8').toString();
 
 const N = Number(input);
 
