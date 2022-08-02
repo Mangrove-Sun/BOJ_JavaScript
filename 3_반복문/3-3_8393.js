9323477a05d59661d3005914e0bff7ebdb514b25
Mangrove-Sun <tjsghd0317@gmail.com>
2022-08-02 21:51:03 +0900

    No.8393 solution

---
+++ 3_반복문/3-3_8393.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const input = fs.readFileSync('input.txt', 'utf8').toString();
+
+const N = Number(input);
+
+let sum = 0;
+for (let i = 1; i <= N; i += 1) {
+  sum += i;
+}
+console.log(sum);
