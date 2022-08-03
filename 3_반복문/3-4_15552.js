ca1ff7f5c2117b30c1696236e289ab46ea4cb98d
Mangrove-Sun <tjsghd0317@gmail.com>
2022-08-03 14:54:17 +0900

    No.15552 solution

---
+++ 3_반복문/3-4_15552.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+
+// 인코딩 방식을 명시해 주면 .toString()을 생략해도 된다.
+// 반대로 인코딩 방식이 명시되어있지 않으면 .toString()을 사용해야한다.
+const input = fs.readFileSync('/dev/stdin', 'utf8').split('\n');
+
+const count = Number(input[0]);
+
+let answer = '';
+
+for (let i = 1; i <= count; i += 1) {
+  const n = input[i].split(' ');
+
+  answer += Number(n[0]) + Number(n[1]) + '\n';
+}
+
+console.log(answer)
+
+// 메모리 164904KB / 시간 1288ms 
