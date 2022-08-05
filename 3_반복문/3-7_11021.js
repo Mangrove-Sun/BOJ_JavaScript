94470d81043bde51899103c4e740890c7f96eb7a
Mangrove-Sun <tjsghd0317@gmail.com>
2022-08-05 16:34:01 +0900

    No.11022 solution

--- 3_반복문/3-7_11021.js
+++ 3_반복문/3-7_11021.js
@@ -4,11 +4,11 @@ const fs = require('fs');
 // 반대로 인코딩 방식이 명시되어있지 않으면 .toString()을 사용해야한다.
 const input = fs.readFileSync('/dev/stdin', 'utf8').split('\n');
 
-const count = Number(input[0]);
+const range = Number(input[0]);
 
 let answer = '';
 
-for (let i = 1; i <= count; i += 1) {
+for (let i = 1; i <= range; i += 1) {
   const n = input[i].split(' ');
 
   answer += 'Case ' + '#'+ i + ': ' + (Number(n[0]) + Number(n[1])) + '\n';
--- 3_반복문/3-8_11022.js
+++ 3_반복문/3-8_11022.js
@@ -1,14 +1,14 @@
 const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin', 'utf8').split('\n');
 
-const count = Number(input[0]);
+const range = Number(input[0]);
 
 let answer = '';
 
-for (let i = 1; i <= count; i += 1) {
-  const n = input[i].split(' ');
-
-  answer += 'Case ' + '#'+ i + ': ' + Number(n[0]) + ' + ' + Number(n[1]) + ' = ' +(Number(n[0]) + Number(n[1])) + '\n';
+for (let i = 1; i <= range; i += 1) {
+  answer = '';
+  for (let j = 1; j <= i; j +=1) {
+    answer += '*';
+  }
+  console.log(answer);
 }
-
-console.log(answer);
