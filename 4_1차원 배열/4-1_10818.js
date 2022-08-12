a8f0a3651eb8cd5e31fcd73ca05d408c12977929
Mangrove-Sun <tjsghd0317@gmail.com>
2022-08-12 17:04:46 +0900

    No.10818 solution

---
+++ 4_1차원 배열/4-1_10818.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const input = fs.readFileSync('input.txt').toString().split('\n');
+
+const range = Number(input[0]);
+const nums = input[1].split(' ');
+
+let MAX = -1000000;
+let MIN = 1000000;
+
+for (let i = 0; i < range; i += 1){
+  let value = Number(nums[i]);
+
+  if (value < MIN) MIN = value;
+  if (value > MAX) MAX = value;
+
+}
+console.log(`${MIN} ${MAX}`);
